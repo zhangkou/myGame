7baa55034e79c40de116a2e86037526734e24289
kozhang <870478065@qq.com>
2017-05-04 22:57:32 +0800

    x

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {$WebSocket, WebSocketSendMode} from 'angular2-websocket/angular2-websocket';
 
 @Component({
   selector: 'page-home',
@@ -7,41 +8,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   speed: number = 30 ;
+  ws: $WebSocket ;
 
   constructor(public navCtrl: NavController) {
-
+    this.ws = new $WebSocket("ws://192.168.10.2:8080/ws/WebsocketServer/app");
   }
 
   topstart(){
+    this.sendMessage("goStraight") ;
     console.log("topstart")
   }
 
   topend(){
+    this.sendMessage("stop") ;
     console.log("topend")
   }
 
   bottomstart(){
+    this.sendMessage("goBack") ;
     console.log("bottomstart")
   }
 
   bottomend(){
+    this.sendMessage("stop") ;
     console.log("bottomend")    
   }
 
   leftstart(){
+    this.sendMessage("turnLeft") ;
     console.log("leftstart")
   }
 
   leftend(){
+    this.sendMessage("stop") ;
     console.log("leftend")    
   }
 
   rightstart(){
+    this.sendMessage("turnRight") ;
     console.log("rightstart")
   }
 
   rightend(){
+    this.sendMessage("stop") ;
     console.log("rightend")    
+  }
+
+  sendMessage(act){
+    this.ws.send4Direct(JSON.stringify({
+      action: act,
+      speed: this.speed
+    })) ;
   }
 
 }
